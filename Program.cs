ba6e479f836992e0b44d3908c8240835f63f0e9c
winniekwokwy <206894518+winniekwokwy@users.noreply.github.com>
2026-06-09 16:18:29 +0100

    add swagger

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
 
 app.UseHttpsRedirection();
